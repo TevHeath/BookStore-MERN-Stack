77a4fd9da496b2e1c25c8a0715cbb82a20f4562b
tevin <tevinheath.beng@gmail.com>
2024-11-06 17:48:20 -0500

    Error with the app listening

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,5 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:CoCreCo2024%21@books-store-mern.sad6d.mongodb.net/books-collections?retryWrites=true&w=majority&appName=Books-Store-MERN';
+// Please create a free database for yourelf
+// This database will be deleted after tutorial
